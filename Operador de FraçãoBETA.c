6d1eb2fc887a08a157d7ad9b4beaddfb06bf837f
Thiago Jansen Sampaio Costa <thhiaggo1@hotmail.com>
2017-11-23 13:17:23 -0200

    Update Operador de FraçãoBETA.c

--- Operador de FraçãoBETA.c
+++ Operador de FraçãoBETA.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 struct fracao{
     int Nume,Deno;
@@ -7,82 +8,55 @@ struct fracao{
 };
 
 
-struct fracao Mult (struct fracao A, struct fracao B){
-	struct fracao Res;
-	Res.Nume = (A.Nume)*(B.Nume);
-	Res.Deno = (A.Deno)*(B.Deno);
-	return Res;
+struct fracao Mult (struct fracao A, struct fracao B,struct fracao *Res){
+	Res->Nume = (A.Nume)*(B.Nume);
+	Res->Deno = (A.Deno)*(B.Deno);
+
     
 
 }
-struct fracao Div (struct fracao A, struct fracao B){
-	struct fracao Res;
-	Res.Nume = (A.Nume)*(B.Deno);
-	Res.Deno = (A.Deno)*(B.Nume);
-	return Res;
+struct fracao Div (struct fracao A, struct fracao B,struct fracao *Res){
+	Res->Nume = (A.Nume)*(B.Deno);
+	Res->Deno = (A.Deno)*(B.Nume);
     
 
 }
-struct fracao Soma (struct fracao A, struct fracao B){
-	struct fracao Res;
+struct fracao Soma (struct fracao A, struct fracao B,struct fracao *Res){
 	int ParaA,ParaB;
-	Res.Deno = (A.Deno)*(B.Deno);
-	ParaA = ((Res.Deno)/A.Deno);
-	ParaB = ((Res.Deno)/B.Deno);
-	Res.Nume = (ParaA*A.Nume)+(ParaB*B.Nume);
-	return Res;
+	Res->Deno = (A.Deno)*(B.Deno);
+	ParaA = ((Res->Deno)/A.Deno);
+	ParaB = ((Res->Deno)/B.Deno);
+	Res->Nume = (ParaA*A.Nume)+(ParaB*B.Nume);
     
 
 }
-struct fracao Sub(struct fracao A, struct fracao B){
-	struct fracao Res;
+struct fracao Sub(struct fracao A, struct fracao B,struct fracao *Res){
 	int ParaA,ParaB;
-	Res.Deno = (A.Deno)*(B.Deno);
-	ParaA = ((Res.Deno)/A.Deno);
-	ParaB = ((Res.Deno)/B.Deno);
-	Res.Nume = (ParaA*A.Nume)-(ParaB*B.Nume);
-	return Res;
+	Res->Deno = (A.Deno)*(B.Deno);
+	ParaA = ((Res->Deno)/A.Deno);
+	ParaB = ((Res->Deno)/B.Deno);
+	Res->Nume = (ParaA*A.Nume)-(ParaB*B.Nume);
     
-
 }
-struct fracao Reduz (struct fracao A){
-	struct fracao Res;
-	if ((A.Nume%2==0)&&(A.Deno%2==0)){
-		Res.Nume=A.Nume/2;
-		Res.Deno=A.Deno/2;
-		Res = Reduz(Res);
+struct fracao Expo(int A,struct fracao *Res){
+	Res->Deno = pow(Res->Deno,A);
+	Res->Nume = pow(Res->Nume,A);
+    
+}
+struct fracao Reduz (struct fracao *Res){
+	int Primos[25]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+	int i=0;
+	while(i<25){
+		if ((Res->Nume%Primos[i]==0)&&(Res->Deno%Primos[i]==0)){
+		Res->Nume=Res->Nume/Primos[i];
+		Res->Deno=Res->Deno/Primos[i];
+		Reduz(Res);
+		}
+	i++;
 	}
-	else if ((A.Nume%3==0)&&(A.Deno%3==0)){
-		Res.Nume=A.Nume/3;
-		Res.Deno=A.Deno/3;
-		Res = Reduz(Res);
-	}
-	else if ((A.Nume%5==0)&&(A.Deno%5==0)){
-		Res.Nume=A.Nume/5;
-		Res.Deno=A.Deno/5;
-		Res = Reduz(Res);
-	}
-	else if ((A.Nume%7==0)&&(A.Deno%7==0)){
-		Res.Nume=A.Nume/7;
-		Res.Deno=A.Deno/7;
-		Res =  Reduz(Res);
-	}
-	else if ((A.Nume%11==0)&&(A.Deno%11==0)){
-		Res.Nume=A.Nume/11;
-		Res.Deno=A.Deno/11;
-		Res =  Reduz(Res);
-	}
-	else if ((A.Nume%13==0)&&(A.Deno%13==0)){
-		Res.Nume=A.Nume/13;
-		Res.Deno=A.Deno/13;
-		Res =  Reduz(Res);
-		
-	}
-	else {
-		Res.Nume=A.Nume;
-		Res.Deno=A.Deno;
-		return Res;
-	}
+
+	Res->Nume=Res->Nume;
+	Res->Deno=Res->Deno;
 }
 
 int main (){
@@ -90,78 +64,72 @@ int main (){
     struct fracao fracaoA;
     struct fracao fracaoB;
     struct fracao Resultado;
-    int L = 0, A, T = 0, K;
-    int Cont;
+    int L = 0, T = 0, K;
+    int Expoe;
 	while (L == 0){
-    	printf("Escolha a operacao desejada\n 1 - Mult 2 - Div \n 3- Soma 4 - Subtracao 5 - reduzir\n");
+    	printf("Escolha a operacao desejada\n 1- Mult 2- Div \n 3- Soma 4- Subtracao\n 5- reduzir 6- Expoente \n");
     	scanf("%d",&Para);
-    	if(Para == 5){
+    	if((Para == 5)||(Para == 6)){
     		printf("Digite o Numerado e Denominador da 1\n");
-   			scanf("%d %d",&fracaoA.Nume,&fracaoA.Deno);		
+   			scanf("%d %d",&fracaoA.Nume,&fracaoA.Deno);
+			   if(Para == 6){
+			   	printf("Digite o expoente\n");
+   				scanf("%d",&Expoe);
+			   }		
 		}
 		else{
 			printf("Digite o Numerado e Denominador da 1\n");
    			scanf("%d %d",&fracaoA.Nume,&fracaoA.Deno);
     		printf("Digite o Numerado e Denominador da 2\n");
     		scanf("%d %d",&fracaoB.Nume,&fracaoB.Deno);
-		}
+			}
 		T = 0;
-		Cont = 0;
 		while(T == 0){
     		if(Para == 1){
-            	Resultado = Mult(fracaoA,fracaoB);
+            	Mult(fracaoA,fracaoB,&fracaoA);
             	printf("Resultado\n");
-				printf("%d/",Resultado.Nume);
-				printf("%d\n",Resultado.Deno);
+				printf("%d/",fracaoA.Nume);
+				printf("%d\n",fracaoA.Deno);
 				Para = 5;
-				Cont ++;
 			}
     		else if(Para == 2){
-   				Resultado = Div(fracaoA,fracaoB);
+   				Div(fracaoA,fracaoB,&fracaoA);
    				printf("Resultado\n");
-   				printf("%d/",Resultado.Nume);
-				printf("%d\n",Resultado.Deno);
+   				printf("%d/",fracaoA.Nume);
+				printf("%d\n",fracaoA.Deno);
 				Para = 5;
-				Cont ++;
 			}
     		else if(Para == 3){
-        		Resultado = Soma(fracaoA,fracaoB);
+        		Soma(fracaoA,fracaoB,&fracaoA);
         		printf("Resultado\n");
-        		printf("%d/",Resultado.Nume);
-				printf("%d\n",Resultado.Deno);
+        		printf("%d/",fracaoA.Nume);
+				printf("%d\n",fracaoA.Deno);
 				Para = 5;
-				Cont ++;
     		}
 			else if(Para == 4){
-        		Resultado = Sub(fracaoA,fracaoB);
+        		Sub(fracaoA,fracaoB,&fracaoA);
         		printf("Resultado\n");
-        		printf("%d/",Resultado.Nume);
-				printf("%d\n",Resultado.Deno);
+        		printf("%d/",fracaoA.Nume);
+				printf("%d\n",fracaoA.Deno);
 				Para = 5;
-				Cont ++;
+			}
+			else if(Para == 6){
+        		Expo(Expoe,&fracaoA);
+        		printf("Resultado\n");
+        		printf("%d/",fracaoA.Nume);
+				printf("%d\n",fracaoA.Deno);
+				Para = 5;
+		
 			}
     		else if(Para == 5){
-    			if (Cont == 0){
-    				Resultado = Reduz(fracaoA);
+    				Reduz(&fracaoA);
     				printf("Forma Reduzida\n");
-					if(Resultado.Deno == 1){
-						printf("%d\n",Resultado.Nume);
-		 			}
-					else{
-						printf("%d/",Resultado.Nume);
-						printf("%d\n",Resultado.Deno);
-					}
-				}
+				if(fracaoA.Deno == 1){
+					printf("%d\n",fracaoA.Nume);
+		 		}
 				else{
-					Resultado = Reduz(Resultado);
-					printf("Forma Reduzida\n");
-					if(Resultado.Deno == 1){
-						printf("%d\n",Resultado.Nume);
-		 			}
-					else{
-						printf("%d/",Resultado.Nume);
-						printf("%d\n",Resultado.Deno);
-					}
+					printf("%d/",fracaoA.Nume);
+					printf("%d\n",fracaoA.Deno);
 				}
 				T = 1;
 			}
