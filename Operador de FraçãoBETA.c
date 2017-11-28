0b5ff0bb12369c669d5b7a00ace7a2c19fb60624
Thiago Jansen Sampaio Costa <thhiaggo1@hotmail.com>
2017-11-28 13:06:54 -0200

    Update Operador de FraçãoBETA.c

--- Operador de FraçãoBETA.c
+++ Operador de FraçãoBETA.c
@@ -65,12 +65,31 @@ int main (){
     struct fracao fracaoB;
     struct fracao Resultado;
     int L = 0, T = 0, K;
-    int Expoe;
+    int Expoe, Cont =0;
+    char Resultados[50][6];
+    int Parame[50];
+    char Ope[7] = {'*','/','+','-'};
 	while (L == 0){
-    	printf("Escolha a operacao desejada\n 1- Mult 2- Div \n 3- Soma 4- Subtracao\n 5- reduzir 6- Expoente \n");
-    	scanf("%d",&Para);
+    	if (Cont!=0){
+    		printf("%d-Operacoes passadas:\n",1);
+    		int j = 0;
+    		for(;j<Cont;j++){
+    			if (Parame[j]!=5 && Parame[j]!=6){
+					printf("%d-%d/%d%c%d/%d = %d/%d\n ",j,Resultados[j][0],Resultados[j][1],Ope[Parame[j]-1],Resultados[j][2],Resultados[j][3],Resultados[j][4],Resultados[j][5]);						
+				}
+				else if(Parame[j]==6){
+					printf("%d-%d/%d**%d = %d/%d\n",j,Resultados[j][0],Resultados[j][1],Resultados[j][2],Resultados[j][3],Resultados[j][4]);
+					
+				}
+				else{
+					printf("%d-%d/%d->%d/%d\n",j,Resultados[j][0],Resultados[j][1],Resultados[j][2],Resultados[j][3]);
+				}
+			}
+		}
+    	printf("Escolha a operacao desejada\n 1- Mult 2- Div \n 3- Soma 4- Subtracao\n 5- Fatoracao 6- Elevar \n");
+    	scanf("%d",&Para);	
     	if((Para == 5)||(Para == 6)){
-    		printf("Digite o Numerado e Denominador da 1\n");
+    		printf("Digite o Numerado e Denominador da 1 fracao \n");
    			scanf("%d %d",&fracaoA.Nume,&fracaoA.Deno);
 			   if(Para == 6){
 			   	printf("Digite o expoente\n");
@@ -78,52 +97,93 @@ int main (){
 			   }		
 		}
 		else{
-			printf("Digite o Numerado e Denominador da 1\n");
+			printf("Digite o Numerado e Denominador da 1 fracao\n");
    			scanf("%d %d",&fracaoA.Nume,&fracaoA.Deno);
-    		printf("Digite o Numerado e Denominador da 2\n");
+    		printf("Digite o Numerado e Denominador da 2 fracao\n");
     		scanf("%d %d",&fracaoB.Nume,&fracaoB.Deno);
 			}
 		T = 0;
 		while(T == 0){
     		if(Para == 1){
+				Resultados[Cont][0] = fracaoA.Nume;
+            	Resultados[Cont][1] = fracaoA.Deno;
+            	Resultados[Cont][2] = fracaoB.Nume;
+            	Resultados[Cont][3] = fracaoB.Deno;
             	Mult(fracaoA,fracaoB,&fracaoA);
             	printf("Resultado\n");
 				printf("%d/",fracaoA.Nume);
 				printf("%d\n",fracaoA.Deno);
+				Parame[Cont] = Para;
+				Resultados[Cont][4] = fracaoA.Nume;
+				Resultados[Cont][5] = fracaoA.Deno;
 				Para = 5;
+				Cont++;
 			}
     		else if(Para == 2){
+    			Resultados[Cont][0] = fracaoA.Nume;
+            	Resultados[Cont][1] = fracaoA.Deno;
+            	Resultados[Cont][2] = fracaoB.Nume;
+            	Resultados[Cont][3] = fracaoB.Deno;
    				Div(fracaoA,fracaoB,&fracaoA);
    				printf("Resultado\n");
    				printf("%d/",fracaoA.Nume);
 				printf("%d\n",fracaoA.Deno);
+				Parame[Cont] = Para;
+				Resultados[Cont][4] = fracaoA.Nume;
+				Resultados[Cont][5] = fracaoA.Deno;
 				Para = 5;
+				Cont++;
 			}
     		else if(Para == 3){
+    			Resultados[Cont][0] = fracaoA.Nume;
+            	Resultados[Cont][1] = fracaoA.Deno;
+            	Resultados[Cont][2] = fracaoB.Nume;
+            	Resultados[Cont][3] = fracaoB.Deno;
         		Soma(fracaoA,fracaoB,&fracaoA);
         		printf("Resultado\n");
         		printf("%d/",fracaoA.Nume);
 				printf("%d\n",fracaoA.Deno);
+				Parame[Cont] = Para;
+				Resultados[Cont][4] = fracaoA.Nume;
+				Resultados[Cont][5] = fracaoA.Deno;
 				Para = 5;
+				Cont++;
     		}
 			else if(Para == 4){
+				Resultados[Cont][0] = fracaoA.Nume;
+            	Resultados[Cont][1] = fracaoA.Deno;
+            	Resultados[Cont][2] = fracaoB.Nume;
+            	Resultados[Cont][3] = fracaoB.Deno;
         		Sub(fracaoA,fracaoB,&fracaoA);
         		printf("Resultado\n");
         		printf("%d/",fracaoA.Nume);
 				printf("%d\n",fracaoA.Deno);
+				Parame[Cont] = Para;
+				Resultados[Cont][4] = fracaoA.Nume;
+				Resultados[Cont][5] = fracaoA.Deno;
 				Para = 5;
+				Cont++;
 			}
 			else if(Para == 6){
+				Resultados[Cont][0] = fracaoA.Nume;
+            	Resultados[Cont][1] = fracaoA.Deno;
+            	Resultados[Cont][2] = Expoe;
         		Expo(Expoe,&fracaoA);
         		printf("Resultado\n");
         		printf("%d/",fracaoA.Nume);
 				printf("%d\n",fracaoA.Deno);
+				Parame[Cont] = Para;
+				Resultados[Cont][3] = fracaoA.Nume;
+				Resultados[Cont][4] = fracaoA.Deno;
 				Para = 5;
+				Cont++;
 		
 			}
     		else if(Para == 5){
-    				Reduz(&fracaoA);
-    				printf("Forma Reduzida\n");
+    			Resultados[Cont][0] = fracaoA.Nume;
+            	Resultados[Cont][1] = fracaoA.Deno;
+    			Reduz(&fracaoA);
+    			printf("Forma Reduzida\n");
 				if(fracaoA.Deno == 1){
 					printf("%d\n",fracaoA.Nume);
 		 		}
@@ -131,8 +191,13 @@ int main (){
 					printf("%d/",fracaoA.Nume);
 					printf("%d\n",fracaoA.Deno);
 				}
+				Parame[Cont] = Para;
+				Resultados[Cont][2] = fracaoA.Nume;
+				Resultados[Cont][3] = fracaoA.Deno;
 				T = 1;
+				Cont++;
 			}
+			
 		}
 		printf("1 p/reiniciar 2 p/parar\n");
 		scanf("%d",&K);
